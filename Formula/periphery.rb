1227d8a5b23deb6880adfaf28e900776a2f7e0f4
mbecker20 <becker.maxh@gmail.com>
2024-11-01 01:13:04 -0400

    update formula

--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -3,19 +3,21 @@ class Periphery < Formula
   homepage "https://komo.do"
   url "https://github.com/mbecker20/homebrew-komodo/releases/download/v1.16.7/periphery.tar.gz"
   sha256 "0f7fa957b71bd47877749fbef39e8ce692487d6cf5699861780d07a6fcf4a1e7"
-  license "MIT"  # Replace with your project's license
+  license "GPL-V3.0"
 
   def install
     bin.install "periphery"
     (etc/"komodo").install "periphery.config.toml"
     (var/"komodo").mkpath
+    (var/"log/komodo").mkpath
   end
 
   # Define a launchd service block
   service do
-    run ["/bin/sh", "-c", "export PATH=$PATH:/usr/local/bin && #{opt_bin}/periphery --config-path #{etc}/komodo/periphery.config.toml"]
+    run [opt_bin/"periphery", "--config-path", etc/"komodo/periphery.config.toml"]
     keep_alive true
     working_dir var/"komodo"
+    environment_variables PATH: std_path
     log_path var/"log/komodo/periphery.log"
     error_log_path var/"log/komodo/periphery-error.log"
   end
