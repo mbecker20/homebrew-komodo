ad85950c5785ff3a6ddb69d375ccdeb2d82672af
mbecker20 <becker.maxh@gmail.com>
2024-11-05 00:25:04 -0500

    1.16.9

--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -1,7 +1,7 @@
 class Periphery < Formula
   desc "Agent to connect with Komodo Core"
   homepage "https://komo.do"
-  url "https://github.com/mbecker20/homebrew-komodo/releases/download/v1.16.8/periphery.tar.gz"
+  url "https://github.com/mbecker20/homebrew-komodo/releases/download/v1.16.9/periphery.tar.gz"
   sha256 "70b307d7d41a799cad9510ef13e3daf149892739b876e86b8806bfbbbd3040fe"
   license "GPL-V3.0"
 
